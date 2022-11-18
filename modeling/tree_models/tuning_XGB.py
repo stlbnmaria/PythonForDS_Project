ee1238377da91ffae0524a85d992272d79ea87ba
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-18 22:16:50 +0100

    added num_cols selction to KNN and linear models, black formatting

--- modeling/tree_models/tuning_XGB.py
+++ modeling/tree_models/tuning_XGB.py
@@ -8,13 +8,13 @@ from tuning import tuning_estimator
 if __name__ == "__main__":
     estimator = XGBRegressor()
     grid = {
-        "xgbregressor__n_estimators": [100, 200],        
-        "xgbregressor__colsample_bytree": [.8, 1],
-        "xgbregressor__colsample_bylevel": [.8, 1],
-        "xgbregressor__learning_rate": [.01, .1, .2],
+        "xgbregressor__n_estimators": [100, 200],
+        "xgbregressor__colsample_bytree": [0.8, 1],
+        "xgbregressor__colsample_bylevel": [0.8, 1],
+        "xgbregressor__learning_rate": [0.01, 0.1, 0.2],
         "xgbregressor__max_depth": [4, 6, 8],
-        "xgbregressor__reg_alpha": [0, .01],
-        "xgbregressor__subsample": [.6, .8, 1],
+        "xgbregressor__reg_alpha": [0, 0.01],
+        "xgbregressor__subsample": [0.6, 0.8, 1],
     }
     path = "./results_tuning"
     filename = "tuning_xgb"
