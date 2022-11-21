10ae16740c76e27d029b75cd42cee01415fed29f
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-21 19:17:22 +0100

    added new grid XGB tuning and black format

--- modeling/tree_models/tuning_XGB.py
+++ modeling/tree_models/tuning_XGB.py
@@ -7,15 +7,17 @@ from tuning import tuning_estimator
 
 if __name__ == "__main__":
     estimator = XGBRegressor()
+
     grid = {
-        "xgbregressor__n_estimators": [100, 200],
-        "xgbregressor__colsample_bytree": [0.8, 1],
-        "xgbregressor__colsample_bylevel": [0.8, 1],
-        "xgbregressor__learning_rate": [0.01, 0.1, 0.2],
-        "xgbregressor__max_depth": [4, 6, 8],
-        "xgbregressor__reg_alpha": [0, 0.01],
-        "xgbregressor__subsample": [0.6, 0.8, 1],
+        "xgbregressor__n_estimators": [400],
+        "xgbregressor__learning_rate": [0.1],
+        "xgbregressor__colsample_bytree": [0.5, 0.6, 0.7],
+        "xgbregressor__colsample_bylevel": [0.5, 0.6, 0.7],
+        "xgbregressor__colsample_bynode": [0.5, 0.6, 0.7],
+        "xgbregressor__max_depth": [9, 10],
+        "xgbregressor__subsample": [0.8],
     }
+
     path = "./results_tuning"
     filename = "tuning_xgb"
     tuning_estimator(estimator, grid, path, filename)
