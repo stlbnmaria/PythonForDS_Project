deca0e2228ace9662c6607c0bdc436b628db0823
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-23 11:11:27 +0100

    added lightgbm to tree_trials, analysed more tuning in tree_models, ran new tuning grid xgb

--- modeling/tree_models/tuning_XGB.py
+++ modeling/tree_models/tuning_XGB.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     estimator = XGBRegressor(tree_method="hist")
 
     grid = {
-        "xgbregressor__n_estimators": [600],
+        "xgbregressor__n_estimators": [300],
         "xgbregressor__learning_rate": [0.1],
-        "xgbregressor__colsample_bytree": [.4, .5, .6,],
-        "xgbregressor__colsample_bylevel": [.6, .8],
-        "xgbregressor__colsample_bynode": [.8, .9],
+        "xgbregressor__colsample_bytree": [.6, .7, .8],
+        "xgbregressor__colsample_bylevel": [.5, .6],
+        "xgbregressor__colsample_bynode": [.6, .8],
         "xgbregressor__max_depth": [4, 6, 8, 10],
         "xgbregressor__subsample": [.6, .7, .8, .9, 1],
         "xgbregressor__min_child_weight": [1, 10],
     }
 
     path = "./results_tuning"
-    filename = "tuning_xgb_reduced_higher"
-    tuning_estimator(estimator, grid, path, filename, n_jobs=6)
+    filename = "tuning_xgb_reduced_lower"
+    tuning_estimator(estimator, grid, path, filename, n_jobs=4)
