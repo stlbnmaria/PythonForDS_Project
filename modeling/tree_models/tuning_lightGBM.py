edecaaa9c3c34c6600b4922d965ed5ee973b5a15
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-24 17:21:31 +0100

    changed tuning grid of light gbm

--- modeling/tree_models/tuning_lightGBM.py
+++ modeling/tree_models/tuning_lightGBM.py
@@ -9,17 +9,15 @@ if __name__ == "__main__":
     estimator = LGBMRegressor()
 
     grid = {
-        "lgbmregressor__n_estimators": [300],
-        "lgbmregressor__subsample": [.6, .7, .8, .9, 1],
-
-        "lgbmregressor__learning_rate": [0.1],
-        "lgbmregressor__colsample_bytree": [.5, .6, .7, .8],
-        "lgbmregressor__colsample_bynode": [.6, .8, .9],
-        "lgbmregressor__max_depth": [4, 6, 8, 10],
-
-        "lgbmregressor__min_child_weight": [0.1, 1],
+        "lgbmregressor__n_estimators": [400, 600, 800], 
+        "lgbmregressor__learning_rate": [.1, .05],
+        "lgbmregressor__num_leaves": [30, 50, 70],
+        "lgbmregressor__min_child_samples": [10, 20, 30],
+        "lgbmregressor__subsample": [.6, .8, 1],
+        "lgbmregressor__subsample_freq": [1, 10],
+        "lgbmregressor__colsample_bytree": [.4, .6, .8, 1],
     }
 
     path = "./results_tuning"
     filename = "tuning_lightGBM"
-    tuning_estimator(estimator, grid, path, filename, n_jobs=4)
+    tuning_estimator(estimator, grid, path, filename, n_jobs=6)
