bec21a371265b3d2c32bee0dff6b8ed27094daa0
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-26 23:50:23 +0100

    adjusted grid after run without lon/lat

--- modeling/tree_models/tuning_cat_no_monkey.py
+++ modeling/tree_models/tuning_cat_no_monkey.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         "catboostregressor__iterations": [1000, 1200],
         "catboostregressor__subsample": [0.6, 0.7, 0.8],
         "catboostregressor__sampling_frequency": ["PerTree"],
-        "catboostregressor__depth":  [6, 8, 9, 10],
-        "catboostregressor__rsm":  [0.1, 0.15, 0.2, 0.25, 0.3],
+        "catboostregressor__depth":  [9, 10, 11],
+        "catboostregressor__rsm":  [0.05, 0.1, 0.15, 0.2, 0.25],
     }
 
     path = "./results_tuning"
