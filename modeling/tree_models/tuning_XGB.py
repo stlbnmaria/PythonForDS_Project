621c6ff5cf9966b02df354e66d8b3ac982c2f7e6
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-26 22:11:54 +0100

    changed xgb tuning for post feature processing run

--- modeling/tree_models/tuning_XGB.py
+++ modeling/tree_models/tuning_XGB.py
@@ -12,13 +12,12 @@ if __name__ == "__main__":
         "xgbregressor__n_estimators": [300],
         "xgbregressor__learning_rate": [0.1],
         "xgbregressor__colsample_bytree": [0.6, 0.7, 0.8],
-        "xgbregressor__colsample_bylevel": [0.5, 0.6],
-        "xgbregressor__colsample_bynode": [0.6, 0.8],
-        "xgbregressor__max_depth": [4, 6, 8, 10],
-        "xgbregressor__subsample": [0.6, 0.7, 0.8, 0.9, 1],
-        "xgbregressor__min_child_weight": [1, 10],
+        "xgbregressor__colsample_bylevel": [0.5, 0.6, 0.7],
+        "xgbregressor__colsample_bynode": [0.5, 0.6, 0.7],
+        "xgbregressor__max_depth": [6, 8, 10],
+        "xgbregressor__subsample": [0.6, 0.7, 0.8],
     }
 
     path = "./results_tuning"
-    filename = "tuning_xgb_reduced_lower"
+    filename = "tuning_xgb_v2data"
     tuning_estimator(estimator, grid, path, filename, n_jobs=4)
