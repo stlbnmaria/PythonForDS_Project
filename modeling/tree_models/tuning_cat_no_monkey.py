617a38fc452c5dbef1206f0741c6dca3f6a2ca8a
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-26 21:23:08 +0100

    changed cat tuning after feature processing in tuning.py

--- modeling/tree_models/tuning_cat_no_monkey.py
+++ modeling/tree_models/tuning_cat_no_monkey.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     estimator = CatBoostRegressor()
 
     grid = {
-        "catboostregressor__iterations": [500, 750, 1000, 1200, 1400],
-        "catboostregressor__subsample": [0.5, 0.6, 0.7, 0.8, 0.9],
+        "catboostregressor__iterations": [1000, 1200],
+        "catboostregressor__subsample": [0.6, 0.7, 0.8],
         "catboostregressor__sampling_frequency": ["PerTree"],
-        "catboostregressor__depth":  [6, 7, 8, 9, 10],
-        "catboostregressor__rsm":  [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5],
+        "catboostregressor__depth":  [6, 8, 9, 10],
+        "catboostregressor__rsm":  [0.1, 0.15, 0.2, 0.25, 0.3],
     }
 
     path = "./results_tuning"
-    filename = "tuning_cat_no_monkey"
+    filename = "tuning_cat_no_mo_v2data"
     tuning_estimator(estimator, grid, path, filename, n_jobs=-1)
