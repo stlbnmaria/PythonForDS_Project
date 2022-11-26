026115674c277ed09dcb4fcf1f7f72ef275eb2b4
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-26 22:12:26 +0100

    changed lightgbm tuning for post feature processing run

--- modeling/tree_models/tuning_lightGBM.py
+++ modeling/tree_models/tuning_lightGBM.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     estimator = LGBMRegressor()
 
     grid = {
-        "lgbmregressor__n_estimators": [400, 600, 800],
-        "lgbmregressor__learning_rate": [0.1, 0.05],
-        "lgbmregressor__num_leaves": [30, 50, 70],
-        "lgbmregressor__min_child_samples": [10, 20, 30],
-        "lgbmregressor__subsample": [0.6, 0.8, 1],
-        "lgbmregressor__subsample_freq": [1, 10],
-        "lgbmregressor__colsample_bytree": [0.4, 0.6, 0.8, 1],
+        "lgbmregressor__n_estimators": [600, 800, 1000],
+        "lgbmregressor__learning_rate": [0.1],
+        "lgbmregressor__num_leaves": [10, 20, 30],
+        "lgbmregressor__min_child_samples": [5, 10, 20],
+        "lgbmregressor__subsample": [0.6, 0.7, 0.8],
+        "lgbmregressor__subsample_freq": [1, 5],
+        "lgbmregressor__colsample_bytree": [0.1, 0.2, 0.3, 0.4],
     }
 
     path = "./results_tuning"
