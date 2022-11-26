64408d3425cbf9247c22d349f094dadfd5b4624a
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-26 20:32:49 +0100

    changed to tuning without the learning rate

--- modeling/tree_models/tuning_cat_no_monkey.py
+++ modeling/tree_models/tuning_cat_no_monkey.py
@@ -9,14 +9,13 @@ if __name__ == "__main__":
     estimator = CatBoostRegressor()
 
     grid = {
-        "catboostregressor__iterations": [500, 750, 1000],
-        "catboostregressor__learning_rate": [0.05, 0.1, 0.2],
-        "catboostregressor__subsample": [0.4, 0.5, 0.6, 0.7],
+        "catboostregressor__iterations": [500, 750, 1000, 1200, 1400],
+        "catboostregressor__subsample": [0.5, 0.6, 0.7, 0.8, 0.9],
         "catboostregressor__sampling_frequency": ["PerTree"],
-        "catboostregressor__depth": [4, 5, 6, 7],
-        "catboostregressor__rsm": [0.4, 0.5, 0.6, 0.7],
+        "catboostregressor__depth":  [6, 7, 8, 9, 10],
+        "catboostregressor__rsm":  [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5],
     }
 
     path = "./results_tuning"
     filename = "tuning_cat_no_monkey"
-    tuning_estimator(estimator, grid, path, filename, n_jobs=6)
+    tuning_estimator(estimator, grid, path, filename, n_jobs=-1)
