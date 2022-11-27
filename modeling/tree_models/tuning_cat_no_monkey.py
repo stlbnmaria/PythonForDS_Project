154d8a8c303f61d91282de33491a5495ad4b9f07
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2022-11-27 17:42:52 +0100

    changed black

--- modeling/tree_models/tuning_cat_no_monkey.py
+++ modeling/tree_models/tuning_cat_no_monkey.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         "catboostregressor__iterations": [1000, 1200],
         "catboostregressor__subsample": [0.7, 0.8, 0.9],
         "catboostregressor__sampling_frequency": ["PerTree"],
-        "catboostregressor__depth":  [10, 11, 12],
-        "catboostregressor__rsm":  [0.05, 0.06, 0.07, 0.08],
+        "catboostregressor__depth": [10, 11, 12],
+        "catboostregressor__rsm": [0.05, 0.06, 0.07, 0.08],
     }
 
     path = "./results_tuning"
